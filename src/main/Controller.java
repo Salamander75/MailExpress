f885d5d169ce02d206ddf19268e9eef67db2fef3
Salamander75 <jurgensonkarlman@gmail.com>
2017-03-20 23:20:27 +0200

    Develop initial UI #1

---
+++ src/main/Controller.java
@@ -0,0 +1,14 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class Controller {
+    @FXML
+    private Text actiontarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Mail sent");
+    }
+}
