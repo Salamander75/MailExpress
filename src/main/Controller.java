063d5e86fbb62d6ba0655d0abc0cfbeab45e4951
Salamander75 <jurgensonkarlman@gmail.com>
2017-03-22 23:38:45 +0200

    Add maven support and add test mail sending #2

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -2,13 +2,86 @@ package main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 public class Controller {
     @FXML
     private Text actiontarget;
 
+    @FXML
+    private TextField messageContent;
+
+    @FXML
+    private TextField messageSubject;
+
+    @FXML
+    private TextField messageTo;
+
+    @FXML
+    private TextField messageFrom;
+
+    private static String USER_NAME = "jurgensonkarl@gmail.com";  // GMail user name (just the part before "@gmail.com")
+    private static String PASSWORD = "Ventnor781h"; // GMail password
+    private static String RECIPIENT = "jkarl@hot.ee";
+
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         actiontarget.setText("Mail sent");
+        String from = USER_NAME;
+        String pass = PASSWORD;
+        String[] to = { RECIPIENT }; // list of recipient email addresses
+        String subject = "Test stuff";
+        String body = "This is testmail!";
+        sendFromGMail(from, pass, to, subject, body);
+    }
+
+    private void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
     }
 }
